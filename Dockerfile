d53788aa65f930e3a1b64ebe031693e7dafb2e23
Robert Gauld <robert@robertgauld.co.uk>
2018-03-28 10:06:50 +0100

    Fix not correctly creating hard links.

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,12 @@ FROM ubuntu:16.04
 
 LABEL maintainer="Robert Gauld <robert@robertgauld.co.uk>"
 
+# Update and install packages
 RUN apt-get update -y
 RUN apt-get install -y offlineimap ca-certificates
 RUN apt-get clean -y
 
+# Copy config and scripts
 COPY offlineimaprc /root/offlineimaprc
 COPY run.sh /root/run.sh
 
--- run.sh
+++ run.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
 # Get latest existing backup
-LATEST=$( ls -1d /media/destination/*/ 2> /dev/null | tail -1 | sed 's/[^0-9\-]//g' )
+echo "Setting up environment"
+LATEST=$( ls -1d /media/destination/????????-???? 2> /dev/null | sed '/^\/media\/destination\/[[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]][[:digit:]]-[[:digit:]][[:digit:]][[:digit:]][[:digit:]]$/!d' | tail -1 | sed 's/[^0-9\-]//g' )
 CURRENT=$( date +%Y%m%d-%H%M )
 
-# Copy lastest backup if exists, otherwise create empty directory
-if [ -e $LATEST ]; then
-	cp -al /media/destination/$LATEST /media/destination/$CURRENT
+# Copy latest backup if exists, otherwise create empty directory
+if [ ! -z $LATEST ]; then
+	echo "Copying latest backup to new ($LATEST --> $CURRENT)"
+	# No idea why the verbose option is needed but without it the hard links aren't made
+	cp -alv /media/destination/$LATEST /media/destination/$CURRENT > /dev/null
 else
+	echo "Creating empty backup ($CURRENT)"
 	mkdir /media/destination/$CURRENT
+	mkdir /media/destination/$CURRENT/mail
+	mkdir /media/destination/$CURRENT/status
 fi
 
+# Create symlink for local status store
+echo "Creating symlink for status"
+if [ -e /root/.offlineimap ]; then
+	rm /root/.offlineimap
+fi
+ln -s /media/destination/$CURRENT/status /root/.offlineimap
+
 # 'Fix' configuration
-cp /root/offlineimaprc /root/.offlineimaprc
 sed -e "s/YOUR_EMAIL_ADDRESS/$EMAIL/" -e "s/YOUR_PASSWORD/$PASSWORD/" \
-  -e "s/GENERATED_DESTINATION/\/media\/destination\/$CURRENT/" \
+  -e "s/GENERATED_DESTINATION/\/media\/destination\/$CURRENT\/mail/" \
   /root/offlineimaprc > /root/.offlineimaprc
 
-
 # Do Sync
+echo "Performing sync"
 exec /usr/bin/offlineimap -c /root/.offlineimaprc
